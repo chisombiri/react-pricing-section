9ca7350207c35ff4890cad622f8eafeae002c5f8
chisombiri <chisombiri@gmail.com>
2022-07-27 15:34:30 +0100

    pricing functionality implemented

--- src/components/sub/Sub.js
+++ src/components/sub/Sub.js
@@ -1,8 +1,52 @@
 import React from "react";
 import "./sub.css";
 
-const Sub = () => {
-  return <div>Sub</div>;
+const Sub = ({
+  plan,
+  theme,
+  price,
+  isBasic,
+  isPro,
+  isMaster,
+  yearly,
+  onBuy,
+}) => {
+  return (
+    <div className="price-box --card">
+      <div className={`box --p2 ${theme}`}>
+        <p className="--text-light">{plan}</p>
+        <h3 className="--text-light">
+          <span>$</span>
+          <span className="price">{price}</span>
+          {yearly && (
+            <p className="--text-light">
+              <del>30% Off</del>
+            </p>
+          )}
+        </h3>
+        {isPro && <p className="--text-light">Everything in basic, and</p>}
+        {isMaster && <p className="--text-light">Everything in pro, and</p>}
+      </div>
+      <div className="features">
+        <ul>
+          {isBasic && <li>Unlimited Screens</li>}
+          {isBasic && <li>500gb bandwith</li>}
+          {isBasic && <li>100gb storage</li>}
+
+          {isPro && <li>Email support</li>}
+          {isPro && <li>Github access</li>}
+          {isPro && <li>15 backups</li>}
+
+          {isMaster && <li>Push Notification</li>}
+          {isMaster && <li>Top support</li>}
+          {isMaster && <li>25 backups</li>}
+        </ul>
+        <button className={`btn ${theme}`} onClick={onBuy}>
+          Get Now
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Sub;
--- src/components/subList/SubList.js
+++ src/components/subList/SubList.js
@@ -1,8 +1,31 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./sub-list.css";
 import Sub from "../sub/Sub";
 
 const SubList = () => {
+  const [basic, setBasic] = useState(8.99);
+  const [pro, setPro] = useState(18.99);
+  const [master, setMaster] = useState(38.99);
+
+  const [yearly, setYearly] = useState(false);
+
+  const handleClick = () => {
+    setYearly(!yearly);
+  };
+
+  //yearly price calculation
+  const calcYearly = (num, percent) => {
+    return ((num * 12) / 100) * percent;
+  };
+
+  useEffect(() => {
+    yearly ? setBasic(calcYearly(basic, 70).toFixed(0)) : setBasic(8.99);
+
+    yearly ? setPro(calcYearly(pro, 70).toFixed(0)) : setPro(18.99);
+
+    yearly ? setMaster(calcYearly(master, 70).toFixed(0)) : setMaster(38.99);
+  }, [yearly]);
+
   return (
     <section className="main">
       <div className="container --center-all">
@@ -10,11 +33,14 @@ const SubList = () => {
           <h1>React Pricing</h1>
           <div className="--line"></div>
 
-          <div className="--flex-center --my-2">
+          <div className="--flex-center --my2">
             <p>Monthly</p>
-            <div>
-              <span className="toggle-btn">
-                <div className="ball"></div>
+            <div className="--mx2">
+              <span
+                className={yearly ? "toggle-btn toggled" : "toggle-btn"}
+                onClick={handleClick}
+              >
+                <div className={yearly ? "ball move" : "ball"}></div>
               </span>
             </div>
             <p>Yearly</p>
@@ -22,7 +48,30 @@ const SubList = () => {
         </div>
 
         <div className="sub-plans">
-          <Sub />
+          <Sub
+            plan={"Basic"}
+            theme={"theme1"}
+            price={basic}
+            isBasic={true}
+            yearly={yearly}
+            onBuy={() => alert(basic)}
+          />
+          <Sub
+            plan={"Pro"}
+            theme={"theme2"}
+            price={pro}
+            isPro={true}
+            yearly={yearly}
+            onBuy={() => alert(pro)}
+          />
+          <Sub
+            plan={"Master"}
+            theme={"theme3"}
+            price={master}
+            isMaster={true}
+            yearly={yearly}
+            onBuy={() => alert(master)}
+          />
         </div>
       </div>
     </section>
